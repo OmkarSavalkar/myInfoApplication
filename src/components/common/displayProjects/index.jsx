50380d0325fea5d196b242a7e5885318d8a4b631
Omkar Savalkar <omkar>
2023-01-02 14:23:40 +0530

    implemented projects section

--- src/components/common/displayProjects/index.jsx
+++ src/components/common/displayProjects/index.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 import styles from "../displayProjects/index.module.scss";
-import laptop from "../../../images/laptop.png";
 import { StyledButton } from "../../../styledComponents";
-import ssImage from "../../../images/ss.png";
+import asee from "../../../images/asee.png";
 import connectwell from "../../../images/connectwell.png";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    width: "80%",
+    height: "70%",
+    top: "50%",
+    left: "56%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-60%, -50%)",
+    overflow: "auto",
+    borderRadius: "20px",
+  },
+};
 
 const DisplayProject = (props) => {
   const { data } = props;
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <>
       <div>
         <img
-          src={data?.image === "asee" ? ssImage : connectwell}
+          src={data?.image === "asee" ? asee : connectwell}
           alt="project pic"
           className={styles["project-image"]}
         />
@@ -21,17 +46,89 @@ const DisplayProject = (props) => {
         <span className={styles["project-name"]}>
           {`${data?.projectName}`.toUpperCase()}
         </span>
-        <div>
+        {/* <div>
           <span className={styles["project-role"]}>Role: {data?.role}</span>
-        </div>
+        </div> */}
         <p>{data?.shortDescription}</p>
         <StyledButton
           buttonMargin={"3px"}
           title="Click to get detailed information about this project"
+          onClick={openModal}
         >
           Overview <i className="fa fa-info-circle" />
         </StyledButton>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button
+          onClick={closeModal}
+          style={{ float: "right", cursor: "pointer" }}
+        >
+          <i className="fa fa-times" />
+        </button>
+        <p style={{ fontSize: "0.8rem", color: "gray" }}>
+          Here you will get all information about this project which includes my
+          Role, Technology Stack, Project Overview and links of self projects
+        </p>
+        <div
+          style={{
+            fontFamily: "Sans-serif",
+            lineHeight: "2",
+            color: "#2F4964",
+          }}
+        >
+          <div className={styles["modal-projectName"]}>
+            {`${data?.projectName}`.toUpperCase()}
+          </div>
+          <div>
+            <span className={styles["modal-sectionHeading"]}>Role: </span>
+            {data?.role}
+          </div>
+          <div>
+            <span className={styles["modal-sectionHeading"]}>
+              Project Overview :{" "}
+            </span>
+            {data?.overviewDescription}
+          </div>
+          <div>
+            <span className={styles["modal-sectionHeading"]}>
+              Technology Stack :{" "}
+            </span>
+            {data?.tools.map((item, index) => {
+              return (
+                <span key={index} className={styles["modal-techStack"]}>
+                  {item}
+                </span>
+              );
+            })}
+          </div>
+          {data && Object.hasOwn(data, "liveLink") ? (
+            <>
+              <div>
+                <span className={styles["modal-sectionHeading"]}>
+                  Live Project Link :
+                  <a
+                    href={data?.liveLink}
+                    target={"_blank"}
+                    style={{ color: "gray" }}
+                  >
+                    {data?.liveLink}
+                  </a>
+                </span>
+              </div>
+            </>
+          ) : (
+            <div>
+              <span className={styles["modal-sectionHeading"]}>Link : </span>{" "}
+              Link cannnot be provided since its client project !
+            </div>
+          )}
+        </div>
+      </Modal>
     </>
   );
 };
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -3,28 +3,43 @@ import SectionTitleComponent from "../common/sectionTitle";
 import styles from "../projects/index.module.scss";
 import projectData from "../../JSON/projects.json";
 import DisplayProject from "../common/displayProjects";
+import { SectionSubTitle } from "../../styledComponents";
 
 const Projects = () => {
   return (
     <div style={{ position: "relative" }}>
       <div>
-        <SectionTitleComponent
-          sectionTitle="Projects"
-          sectionDescription={
-            "Here you will find some of the personal and clients projects that I created with each project containing its overview"
-          }
-        />
-        <SectionTitleComponent
-          sectionDescription={
-            "( This portfolio is also a Responsive web-application project developed using React Js, JavaScript, SASS-CSS, Styled-Components )"
-          }
-        />
+        <div>
+          <SectionTitleComponent
+            sectionTitle="Projects"
+            sectionDescription={
+              "Here you will find some of the personal and clients projects that I created with each project containing its overview"
+            }
+          />
+        </div>
+        <div id="modalMain"></div>
+        <div className={styles["mainProject-grid"]}>
+          {projectData &&
+            projectData?.map((item, index) => {
+              return <DisplayProject data={item} key={index} />;
+            })}
+        </div>
       </div>
-      <div className={styles["mainProject-grid"]}>
-        {projectData &&
-          projectData?.map((item, index) => {
-            return <DisplayProject data={item} key={index} />;
-          })}
+      <div className={styles["this-project"]}>
+        <SectionSubTitle>
+          <img
+            src="data:image/png;base64,R0lGODlhDwAPAKIHAP9mAP/MAMxmAP+ZAP//AMyZAP+ZM////yH/C05FVFNDQVBFMi4wAwEAAAAh+QQFMgAHACwAAAAADwAPAAADKni63P7wiNgmVaJYKsyuQlgYWvhJo6GSJ5ax7dKNsaxpUTbpUCjXl+AiAQAh+QQFMgAHACwAAAAADwAPAAADQHi6B8yDAeJWVLXRlSUJQfc0YDgC6FQOqTMRcBnAMNsMcm5jeB7snI8M2ND8ADibI6JSfmyXCVDaSWEwqJFFmwAAOw=="
+            alt="star emoji"
+            style={{ marginRight: "4px" }}
+          />
+          ( This portfolio is also a Responsive web-application (self project)
+          developed using React Js, JavaScript, SASS-CSS, Styled-Components )
+          <img
+            src="data:image/png;base64,R0lGODlhDwAPAKIHAP9mAP/MAMxmAP+ZAP//AMyZAP+ZM////yH/C05FVFNDQVBFMi4wAwEAAAAh+QQFMgAHACwAAAAADwAPAAADKni63P7wiNgmVaJYKsyuQlgYWvhJo6GSJ5ax7dKNsaxpUTbpUCjXl+AiAQAh+QQFMgAHACwAAAAADwAPAAADQHi6B8yDAeJWVLXRlSUJQfc0YDgC6FQOqTMRcBnAMNsMcm5jeB7snI8M2ND8ADibI6JSfmyXCVDaSWEwqJFFmwAAOw=="
+            alt="star emoji"
+            style={{ marginLeft: "4px" }}
+          />
+        </SectionSubTitle>
       </div>
     </div>
   );
